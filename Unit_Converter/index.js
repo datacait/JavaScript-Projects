8ea1868b27d196f56a84bee5391f11dd6263b67c
datacait <107415627+datacait@users.noreply.github.com>
2022-06-20 22:12:32 -0500

    6.20.22 update
    completed js & cleaned html/css

--- Unit_Converter/index.js
+++ Unit_Converter/index.js
@@ -3,3 +3,23 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("conv-length")
+let volumeEl = document.getElementById("conv-volume")
+let weightEl = document.getElementById("conv-weight")
+
+const meterToFeet = 3.281 
+const literToGallon = 0.264
+const kilogramToPound = 2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value 
+    
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(2)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(2)} meters`
+
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(2)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(2)} liters`
+    
+    weightEl.textContent = `${baseValue} kilograms = ${(baseValue * kilogramToPound).toFixed(2)} lbs | ${baseValue} lbs = ${(baseValue / kilogramToPound).toFixed(2)} kilograms`
+
+})
